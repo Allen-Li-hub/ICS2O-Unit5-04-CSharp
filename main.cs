6e4506140aa637fd2c4dd1b98194966226361b7f
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-12-20 15:11:18 -0800

    change

--- main.cs
+++ main.cs
@@ -1,7 +1,7 @@
 // Created by: Allen Li
 // Created on: Oct 2022
 //
-// This program displays, "Hello, World!"
+// This program displays the discount checker
 
 using System;
 
@@ -9,7 +9,27 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int userAge;
+        string userDay;
+
+        Console.WriteLine("Let's see can you get the discount or not");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter your age, then the date that you want to get the discount: ");
+        Console.WriteLine("");
+        userAge = Convert.ToInt16(Console.ReadLine());
+        userDay = (Console.ReadLine());
+
+        Console.WriteLine(" ");
+        if ((userDay == "Tuesday" || userDay == "Thursday") || (userAge > 12 && userAge < 21))
+        {
+            Console.WriteLine("You get the discount!");
+        }
+        else
+        {
+            Console.WriteLine("Your current conditions cannot get discount.");
+        }
+
         Console.WriteLine("\nDone.");
     }
 }
